93864568d6843af148fc62e2f1653fd9e8557e70
Kesav Viswanadha <vkesav@google.com>
2020-06-05 14:22:47 -0700

    Fix indentation issues

--- test_bert_trainer.py
+++ test_bert_trainer.py
@@ -17,17 +17,18 @@ def get_filtered_nyt_data(data_path):
     return filtered
 
 if __name__ == '__main__':
-	# Read in data. No need to separate training and validation,
-	# BERTTrainer class takes care of that.
-	nyt_data = get_filtered_nyt_data('nyt_data_from_2015.pkl')
+    # Read in data. No need to separate training and validation,
+    # BERTTrainer class takes care of that.
+    nyt_data = get_filtered_nyt_data('nyt_data_from_2015.pkl')
     trainer = BERTTrainer(nyt_data)
     trainer.train_model()
     eval_accuracy_info = trainer.test_model()
-    prin(eval_accuracy_info)
+    print(eval_accuracy_info)
     preds = trainer.predict([
         "Donald Trump",
         "20th century art",
         "Donald Trump likes 20th century art"
     ])
     print(preds)
+
     # can use these predicted values to compute various other eval metrics 
