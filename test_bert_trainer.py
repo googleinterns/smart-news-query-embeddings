f826783719f4adbb8a307cee24bfc3a745c10af3
Kesav Viswanadha <vkesav@google.com>
2020-06-12 15:44:03 -0700

    Factor out model training code in unit tests

--- test_bert_trainer.py
+++ test_bert_trainer.py
@@ -1,6 +1,6 @@
 import unittest
 import time
-import os
+import shutil
 import pandas as pd
 from bert_trainer import BERTTrainer
 from utils import *
@@ -12,7 +12,7 @@ class TestBERT(unittest.TestCase):
         self.output_dir = 'test_{}'.format(str(int(time.time())))
         self.trainer = BERTTrainer(output_dir=self.output_dir)
 
-    def test_train_and_test(self):
+    def train_model(self):
         data = pd.DataFrame({
             'abstract': ['test one', 'test two', 'test three'] * 5,
             'section': ['U.S.', 'Arts', 'U.S.'] * 5,
@@ -20,8 +20,16 @@ class TestBERT(unittest.TestCase):
         data_column = 'abstract'
         label_column = 'section'
 
-        train_features, test_features, _, label_list = train_and_test_features_from_df(data, data_column, label_column, self.trainer.bert_model_hub, self.trainer.max_seq_length)
+        train_features, test_features, _, label_list = train_and_test_features_from_df(
+                data, data_column, label_column, self.trainer.bert_model_hub,
+                self.trainer.max_seq_length)
         self.trainer.train(train_features, label_list)
+
+    def test_train(self):
+        self.train_model()
+
+    def test_train_and_test(self):
+        self.train_model()
         results = self.trainer.test(test_features)
         print('Evaluation results:', results)
         results2 = self.trainer.test(test_features)
@@ -30,9 +38,10 @@ class TestBERT(unittest.TestCase):
         self.assertEqual(eval_acc1, eval_acc2)
         loss1, loss2 = results['loss'], results2['loss']
         self.assertEqual(eval_acc1, eval_acc2)
-        os.rmdir(self.output_dir)
+        shutil.rmtree(self.output_dir)
 
     def test_train_and_predict(self):
+        self.train_model()
         input_sentences = [
             "test four",
             "test one",
