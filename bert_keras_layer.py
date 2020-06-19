1f96ac845394d3f8651d40ce421407bac35e9785
Kesav Viswanadha <vkesav@google.com>
2020-06-19 21:20:42 +0000

    WIP - load/save bert keras models

--- bert_keras_layer.py
+++ bert_keras_layer.py
@@ -16,20 +16,20 @@ def create_bert_layer(model_dir):
 if __name__ == '__main__':
     tokenizer = create_tokenizer('uncased_L-12_H-768_A-12')
 
-    df = get_filtered_nyt_data('nyt_data_from_2015.pkl')
+    df = get_filtered_nyt_data('nyt_data_from_2015.pkl').sample(5000, random_state=42)
     df['category_labels'] = df['section'].astype('category').cat.codes
     train_df, test_df = train_test_split(df, random_state=42)
     train_ids, train_labels = tokenize_data(train_df['abstract'], train_df['category_labels'], tokenizer)
-    test_ids, test_labels = tokenize_data(df['abstract'], train_df['category_labels'], tokenizer)
+    test_ids, test_labels = tokenize_data(test_df['abstract'], test_df['category_labels'], tokenizer)
     bert_layer = create_bert_layer('uncased_L-12_H-768_A-12')
 
     model = tf.keras.Sequential([
         tf.keras.layers.Input(shape=(max_seq_length,), dtype='int32', name='input_ids'),
         bert_layer,
         tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(256, activation=tf.nn.relu),
-        tf.keras.layers.Dropout(0.5),
-        tf.keras.layers.Dense(256, activation=tf.nn.relu),
+        # tf.keras.layers.Dense(256, activation=tf.nn.relu),
+        # tf.keras.layers.Dropout(0.5),
+        # tf.keras.layers.Dense(256, activation=tf.nn.relu),
         tf.keras.layers.Dropout(0.5),
         tf.keras.layers.Dense(classes, activation=tf.nn.softmax)
     ])
@@ -39,4 +39,6 @@ if __name__ == '__main__':
     model.compile(loss='categorical_crossentropy', optimizer=tf.optimizers.Adam(lr=0.00001), metrics=['accuracy'])
     print(model.summary())
 
-    model.fit(train_ids, train_labels, validation_data=(test_ids, test_labels), epochs=5)
+    model.fit(train_ids, train_labels, validation_data=(test_ids, test_labels), epochs=1)
+
+    model.save('bert_keras')
