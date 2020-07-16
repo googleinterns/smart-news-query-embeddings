6ce7ddee65b57a32faeee6ad48ee62f52bea539c
Kesav Viswanadha <vkesav@google.com>
2020-07-16 16:12:05 +0000

    Add batch normalization to two-tower model to fix overfitting

--- smart_news_query_embeddings/models/two_tower_model.py
+++ smart_news_query_embeddings/models/two_tower_model.py
@@ -15,7 +15,7 @@ limitations under the License.
 """
 
 from smart_news_query_embeddings.models.bert_keras_model import BertKerasModel
-from tensorflow.keras.layers import Dense, Input, Flatten, Dropout, concatenate
+from tensorflow.keras.layers import Dense, Input, Flatten, Dropout, concatenate, BatchNormalization
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 
@@ -23,6 +23,7 @@ class TwoTowerModel(BertKerasModel):
 
     def build_model(self):
         self.flatten = Flatten(name="flatten")
+        self.bn = BatchNormalization(name="bn")
         self.dense1_1 = Dense(self.dense_size, activation="relu", name="dense1_1")
         self.dropout1 = Dropout(self.dropout_rate)
         self.dense1_2 = Dense(self.dense_size, activation="relu", name="dense1_2")
@@ -33,7 +34,7 @@ class TwoTowerModel(BertKerasModel):
 
         self.fc_layer = Dense(128, activation="relu", name="final_dense")
         self.output_layer = Dense(2, activation="sigmoid", name="output_dense")
-        self.pre_embedding_layers = [self.bert_layer, self.flatten, self.dense1_1, self.dropout1]
+        self.pre_embedding_layers = [self.bert_layer, self.bn, self.flatten, self.dense1_1, self.dropout1]
         # define two sets of inputs
         # input_ids = Input(shape=(self.max_seq_length,), dtype='int32', name="input_ids")
         # input_labels = Input(shape=(self.num_classes,), dtype='int32', name="input_labels")
@@ -66,6 +67,7 @@ class TwoTowerModel(BertKerasModel):
     def call(self, inputs):
         input_ids, input_labels = inputs
         out1 = self.bert_layer(input_ids)
+        out1 = self.bn(out1)
         out1 = self.flatten(out1)
         out1 = self.dense1_1(out1)
         out1 = self.dropout1(out1)
