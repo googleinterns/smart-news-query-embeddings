b13d078786c8c0693a1f4ec928be7468b80f27e6
Kesav Viswanadha <vkesav@google.com>
2020-07-22 13:33:43 -0700

    Fix caching of training and validation data

--- train_bert_keras_model.py
+++ train_bert_keras_model.py
@@ -17,6 +17,7 @@ limitations under the License.
 import os
 import time
 import argparse
+import numpy as np
 import tensorflow as tf
 from smart_news_query_embeddings.models.bert_keras_model import BertKerasModel
 from smart_news_query_embeddings.preprocessing.bert_tokenizer import *
@@ -38,24 +39,40 @@ if __name__ == '__main__':
     parser.add_argument('--num-train-epochs', '-n', default=3, type=int)
     parser.add_argument('--dense-size', '-d', default=256, type=int)
     parser.add_argument('--save-summary-every', default=100, type=int)
-    parser.add_argument('--output-dir', '-o', default=output_dir, type=str)
+    parser.add_argument('--exp-name', '-e', default=output_dir, type=str)
     parser.add_argument('--training', '-t', default=True, type=bool)
     parser.add_argument('--bert-dir', default='uncased_L-12_H-768_A-12', type=str)
     parser.add_argument('--tail-cutoff', default=0.5, type=float)
     args = parser.parse_args()
-    if not os.path.exists('outputs'):
-        os.mkdir('outputs')
-    out_dir = os.path.join('outputs', args.output_dir)
+    if not os.path.exists('output_models'):
+        os.mkdir('output_models')
+    if not os.path.exists('output_data'):
+        os.mkdir('output_data')
+    out_dir = os.path.join('output_models', args.exp_name)
+    train_data_path = os.path.join('output_data', args.exp_name, 'train_data.npy')
+    train_labels_path = os.path.join('output_data', args.exp_name, 'train_labels.npy')
+    valid_data_path = os.path.join('output_data', args.exp_name, 'valid_data.npy')
+    valid_labels_path = os.path.join('output_data', args.exp_name, 'valid_labels.npy')
 
-    tokenizer = create_tokenizer(args.bert_dir)
+    if not os.path.exists(train_data_path):
+        tokenizer = create_tokenizer(args.bert_dir)
 
-    df = get_filtered_nyt_data_with_scores(DATA_PATH)
-    df['category_labels'] = df['section'].astype('category').cat.codes
-    num_classes = df['category_labels'].max() + 1
-    CUTOFF = int(df.shape[0] * args.tail_cutoff)
-    train_df, test_df = train_test_split(df, random_state=RANDOM_SEED)
-    train_ids, train_labels = tokenize_data(train_df['fixed_abstract'], train_df['category_labels'], tokenizer, args.max_seq_length, num_classes)
-    test_ids, test_labels = tokenize_data(test_df['fixed_abstract'], test_df['category_labels'], tokenizer, args.max_seq_length, num_classes)
+        df = get_filtered_nyt_data_with_scores(DATA_PATH)
+        df['category_labels'] = df['section'].astype('category').cat.codes
+        num_classes = df['category_labels'].max() + 1
+        CUTOFF = int(df.shape[0] * args.tail_cutoff)
+        train_df, test_df = train_test_split(df, random_state=RANDOM_SEED)
+        train_ids, train_labels = tokenize_data(train_df['fixed_abstract'], train_df['category_labels'], tokenizer, args.max_seq_length, num_classes)
+        test_ids, test_labels = tokenize_data(test_df['fixed_abstract'], test_df['category_labels'], tokenizer, args.max_seq_length, num_classes)
+        np.save(train_ids, train_data_path)
+        np.save(test_ids, valid_data_path)
+        np.save(train_labels, train_labels_path)
+        np.save(test_labels, valid_labels_path)
+    else:
+        train_ids = np.load(train_data_path)
+        test_ids = np.load(valid_data_path)
+        train_labels = np.load(train_labels_path)
+        test_labels = np.load(valid_labels_path)
     if not os.path.exists(out_dir):
         model = BertKerasModel(num_classes, bert_dir=args.bert_dir,
             max_seq_length=args.max_seq_length, dense_size=args.dense_size,
--- train_two_tower_model.py
+++ train_two_tower_model.py
@@ -1,7 +1,9 @@
 import os
 import time
 import argparse
+import pickle
 import numpy as np
+import tensorflow as tf
 from smart_news_query_embeddings.models.two_tower_model import TwoTowerModel
 from tensorflow.keras.optimizers import Adam
 
@@ -14,36 +16,68 @@ if __name__ == '__main__':
     parser.add_argument('--dropout-rate', default=0.5, type=float)
     parser.add_argument('--num-train-epochs', '-n', default=3, type=int)
     parser.add_argument('--dense-size', '-d', default=256, type=int)
-    parser.add_argument('--output-dir', '-o', default=output_dir, type=str)
+    parser.add_argument('--exp-name', '-e', default=output_dir, type=str)
     parser.add_argument('--bert-dir', default='uncased_L-12_H-768_A-12', type=str)
     args = parser.parse_args()
+    if not os.path.exists('output_data'):
+        os.mkdir('output_data')
+    exp_data_path = os.path.join('output_data', args.exp_name)
+    if not os.path.exists(exp_data_path):
+        os.mkdir(exp_data_path)
+    train_data_path = os.path.join(exp_data_path, 'train_data.npy')
+    train_labels_path = os.path.join(exp_data_path, 'train_labels.npy')
+    valid_data_path = os.path.join(exp_data_path, 'valid_data.npy')
+    valid_labels_path = os.path.join(exp_data_path, 'valid_labels.npy')
+    if not os.path.exists('output_models'):
+        os.mkdir('output_models')
+    out_dir = os.path.join('output_models', args.exp_name)
 
-    all_train_ids = np.load('data/all_train_ids2.npy')
-    all_train_labels = np.load('data/all_train_labels2.npy')
-    all_train_outputs = np.load('data/all_train_outputs2.npy')
-    all_test_ids = np.load('data/all_test_ids2.npy')
-    all_test_labels = np.load('data/all_test_labels2.npy')
-    all_test_outputs = np.load('data/all_test_outputs2.npy')
+    if not os.path.exists(train_data_path):
+        all_train_ids = np.load('data/all_train_ids.npy')
+        all_train_labels = np.load('data/all_train_labels.npy')
+        all_train_outputs = np.load('data/all_train_outputs.npy')
+        all_valid_ids = np.load('data/all_test_ids.npy')
+        all_valid_labels = np.load('data/all_test_labels.npy')
+        all_valid_outputs = np.load('data/all_test_outputs.npy')
+    else:
+        with open(train_data_path, 'rb') as f:
+            (all_train_ids, all_train_labels) = pickle.load(f)
+        with open(valid_data_path, 'rb') as f:
+            (all_valid_ids, all_valid_labels) = pickle.load(f)
+        with open(train_labels_path, 'rb') as f:
+            all_train_labels = pickle.load(f)
+        with open(valid_labels_path, 'rb') as f:
+            all_valid_labels = pickle.load(f)
+
+    with open(train_data_path, 'wb') as f:
+        pickle.dump((all_train_ids, all_train_labels), f)
+    with open(valid_data_path, 'wb') as f:
+        pickle.dump((all_valid_ids, all_valid_labels), f)
+    with open(train_labels_path, 'wb') as f:
+        pickle.dump(all_train_labels, f)
+    with open(valid_labels_path, 'wb') as f:
+        pickle.dump(all_valid_labels, f)
+    # np.save(train_labels_path, all_train_labels)
+    # np.save(train_outputs_path, all_train_outputs)
+    # np.save(valid_ids_path, (all_valid_ids, all_valid_labels))
+    # np.save(valid_labels_path, all_valid_labels)
+    # np.save(valid_outputs_path, all_valid_outputs)
 
     num_classes = all_train_labels.shape[1]
 
-    model = TwoTowerModel(num_classes, bert_dir=args.bert_dir,
-        max_seq_length=args.max_seq_length, dense_size=args.dense_size, dropout_rate=args.dropout_rate)
+    if not os.path.exists(out_dir):
+        model = TwoTowerModel(num_classes, bert_dir=args.bert_dir,
+            max_seq_length=args.max_seq_length, dense_size=args.dense_size, dropout_rate=args.dropout_rate)
 
-    model.build(input_shape=[(None, 128), (None, 64)])
-    model.compile(loss='binary_crossentropy', optimizer=Adam(lr=args.learning_rate), metrics=['accuracy'])
+        model.build(input_shape=[(None, 128), (None, 64)])
+        model.compile(loss='binary_crossentropy', optimizer=Adam(lr=args.learning_rate), metrics=['accuracy'])
+    else:
+        model = tf.keras.models.load_model(out_dir)
     print(model.summary())
-    if not os.path.exists('output_data'):
-        os.mkdir('output_data')
-    train_data_path = os.path.join('output_data', args.output_dir, 'train_data.npy')
-    valid_data_path = os.path.join('output_data', args.output_dir, 'vaild_data.npy')
 
-    model.fit(x=(all_train_ids, all_train_labels), y=all_train_outputs,
-    	validation_data=((all_test_ids, all_test_labels), all_test_outputs),
+    model.fit(x=(all_train_ids[:80], all_train_labels[:80]), y=all_train_outputs[:80],
+    	validation_data=((all_valid_ids[:20], all_valid_labels[:20]), all_valid_outputs[:20]),
     	epochs=args.num_train_epochs,
     	batch_size=args.batch_size)
 
-    if not os.path.exists('outputs'):
-        os.mkdir('outputs')
-    out_dir = os.path.join('outputs', args.output_dir)
     model.save(out_dir)
